33e10e698272c7fe54ef67255e94854a85867377
Marc Scheib <marc.scheib@gmx.de>
2015-08-25 19:07:19 +0200

    #1: added database configuration

--- config/database.php
+++ config/database.php
@@ -55,9 +55,9 @@ return [
         'mysql' => [
             'driver'    => 'mysql',
             'host'      => env('DB_HOST', 'localhost'),
-            'database'  => env('DB_DATABASE', 'forge'),
-            'username'  => env('DB_USERNAME', 'forge'),
-            'password'  => env('DB_PASSWORD', ''),
+            'database'  => env('DB_DATABASE', 'legends_rising_3'),
+            'username'  => env('DB_USERNAME', 'lr'),
+            'password'  => env('DB_PASSWORD', '7CS2AnCNKxqThQJz'),
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
