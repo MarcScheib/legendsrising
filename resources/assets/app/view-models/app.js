23d3f306a3fb0e883cddfc4a3972a84f26090d49
Marc Scheib <marc.scheib@gmx.de>
2015-09-07 12:08:21 +0200

    #1: added pages view-model
    It is now possible to display static pages via Aurelia by putting the view into the resources/views/pages directory and calling the name via URL.

--- resources/assets/app/view-models/app.js
+++ resources/assets/app/view-models/app.js
@@ -8,8 +8,8 @@ export class App {
         config.title = 'LegendsRising';
         config.options.pushState = true;
         config.map([
-            { route: ['', 'contents', 'contents/index'], name: 'index', moduleId: 'view-models/contents/index', title: 'Welcome' }/**,
-            { route: ['pages/:view'], moduleId: 'pages/index', nav: false },
+            { route: ['', 'contents', 'contents/index'], name: 'index', moduleId: 'view-models/contents/index', title: 'Welcome' },
+            { route: ['pages/:view'], moduleId: 'view-models/pages/index', nav: false }/**,
             { route: ['news', 'news/index'], moduleId: 'news/index', title: 'News' },
             { route: ['news/view/:id'], moduleId: 'news/view', title: 'News' },
             { route: ['users/register'], moduleId: 'users/register', title: 'Registration' },
---
+++ resources/assets/app/view-models/pages/index.js
@@ -0,0 +1,20 @@
+import {activationStrategy} from 'aurelia-router';
+import {hyphenate} from 'aurelia-templating';
+
+export class Index {
+    constructor() {
+
+    }
+
+    determineActivationStrategy() {
+        return activationStrategy.replace;
+    }
+
+    getViewStrategy() {
+        return 'views/pages/' + this.view + '.html';
+    }
+
+    activate(params) {
+        this.view = hyphenate(params.view);
+    }
+}
