8e2955d1fd0142c3016881b5565e4a03a45cfd13
Marc Scheib <marc.scheib@gmx.de>
2015-09-20 20:08:00 +0200

    [#1] news: clean up news controller and add scope
    Removed unnecessary methods from news controller. A scope was added to
    the news model which allows to easy query the most recent news. This is
    currently set to a limit of 5 in the model. This should be moved to an
    option in the future.
    
    ---
    
     app/Http/Controllers/NewsController.php | 66 +-------------------------
     app/Models/News.php                     |  7 +++
     2 files changed, 9 insertions(+), 64 deletions(-)

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -1,83 +1,21 @@
 <?php
 namespace LegendsRising\Http\Controllers;
 
-use Illuminate\Http\Request;
-use LegendsRising\Models\News;
 use LegendsRising\Http\Requests;
+use LegendsRising\Models\News;
 use Log;
 
 class NewsController extends Controller
 {
     public function index()
     {
-        $news = News::with('user')->orderBy('created_at', 'desc')->get();
+        $news = News::recent()->with('user')->get();
 
         return $news;
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  Request  $request
-     * @return Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return Response
-     */
     public function show($id)
     {
         return News::find($id);
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  Request  $request
-     * @param  int  $id
-     * @return Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return Response
-     */
-    public function destroy($id)
-    {
-        //
     }
 }
