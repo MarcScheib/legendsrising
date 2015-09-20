67f9ee3451b189ad6e8f48d4c124e065efbd836e
Marc Scheib <marc.scheib@gmx.de>
2015-09-20 16:13:15 +0200

    [#1] test: cleanup test cases and add controller test
    Adjusted the existing test cases to fulfill the new page layout. Added a
    basic news and user controller test.
    
    ---
    
     tests/ExampleTest.php                         |  7 +------
     tests/Http/Controllers/NewsControllerTest.php | 16 ++++++++++++++++
     tests/Http/Controllers/UserControllerTest.php | 16 ++++++++++++++++
     tests/TestCase.php                            |  2 +-
     4 files changed, 34 insertions(+), 7 deletions(-)

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -6,14 +6,9 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class ExampleTest extends TestCase
 {
-    /**
-     * A basic functional test example.
-     *
-     * @return void
-     */
     public function testBasicExample()
     {
         $this->visit('/')
-             ->see('Laravel 5');
+             ->see('LegendsRising');
     }
 }
