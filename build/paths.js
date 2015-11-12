11b38120afce8ca718af38f5365596f0521c04b0
Marc Scheib <marc.scheib@gmx.de>
2015-11-12 12:34:01 +0100

    [#1] core: fix gulp export-release task
    Gulp export-release task was not working correct because of a missing
    path.
    
    This path is added to the paths.js.
    
    ---
    
     build/paths.js | 2 ++
     1 file changed, 2 insertions(+)

--- build/paths.js
+++ build/paths.js
@@ -2,6 +2,7 @@ var path = require('path');
 
 var appRoot = 'resources/assets/';
 var outputRoot = 'public/assets/';
+var exporSrvtRoot = 'export/'
 
 module.exports = {
   root: appRoot,
@@ -9,6 +10,7 @@ module.exports = {
   html: appRoot + '**/*.html',
   style: appRoot + '**/*.scss',
   output: outputRoot,
+  exportSrv: exporSrvtRoot,
   doc:'./doc',
   e2eSpecsSrc: 'resources/test/e2e/src/*.js',
   e2eSpecsDist: 'resources/test/e2e/dist/'
