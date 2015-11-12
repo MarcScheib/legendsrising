3a149d14f6119c85aa077c40a757d2dac77a537c
Marc Scheib <marc.scheib@gmx.de>
2015-11-12 13:40:56 +0100

    [#1] general: lint js project files
    Lint projects files.
    
    ---
    
     resources/assets/app/view-models/app.js | 9 ++-------
     1 file changed, 2 insertions(+), 7 deletions(-)

--- resources/assets/app/view-models/app.js
+++ resources/assets/app/view-models/app.js
@@ -8,13 +8,8 @@ export class App {
     config.title = 'LegendsRising';
     config.options.pushState = true;
     config.map([
-      {
-        route: ['/', 'contents', 'contents/index'],
-        name: 'index',
-        moduleId: 'view-models/contents/index',
-        title: 'Welcome'
-      },
-      {route: ['pages/:view'], moduleId: 'view-models/pages/index', nav: false},
+      {route: ['/', 'contents', 'contents/index'], name: 'index', moduleId: 'view-models/contents/index', title: 'Welcome'},
+      {route: ['pages/:view'], moduleId: 'view-models/pages/index'},
       {route: ['news', 'news/index'], moduleId: 'view-models/news/index', title: 'News'},
       {route: ['news/view/:id'], moduleId: 'view-models/news/view', title: 'News'},
       {route: ['users/register'], moduleId: 'view-models/users/register', title: 'Registration'},
