83f93303d1e185cf00535df52f97e436ff92d37d
Marc Scheib <marc.scheib@gmx.de>
2015-11-17 21:11:00 +0100

    [#6] auth: rename JWT identifier
    The name of the JWT identifier in frontend and backend were mismatching.
    
    They now have the same name to enable authentication.
    
    ---
    
     assets/app/configuration/auth-config.js | 2 +-
     1 file changed, 1 insertion(+), 1 deletion(-)

--- resources/assets/app/configuration/auth-config.js
+++ resources/assets/app/configuration/auth-config.js
@@ -4,7 +4,7 @@ let authConfig = {
   loginUrl: 'auth/signin',
   // The API serves its tokens with a key of id_token which differs from
   // aurelia-auth's standard
-  tokenName: 'id_token',
+  tokenName: 'token',
   // Once logged in, we want to redirect the user to the welcome view
   loginRedirect: '/'
 };
