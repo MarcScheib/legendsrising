08a21cbf59b3f7cf9226dbec973046e5a95e13f9
Marc Scheib <scheib@infosim.net>
2015-12-01 21:13:16 +0100

    feat(news): show news title on single news view
    The title of a news is shown as the browser title now when displaying a
    single news.
    
    ---
    
     .editorconfig                | 4 ++--
     src/view-models/news/view.js | 7 +++++--
     2 files changed, 7 insertions(+), 4 deletions(-)

--- src/view-models/news/view.js
+++ src/view-models/news/view.js
@@ -7,8 +7,11 @@ export class View {
     this.newsService = newsService;
   }
 
-  activate(params) {
+  activate(params, routeConfig) {
     return this.newsService.get(params.id)
-      .then(news => this.news = news);
+      .then(news => {
+        this.news = news;
+        routeConfig.navModel.setTitle(news.title);
+      });
   }
 }
