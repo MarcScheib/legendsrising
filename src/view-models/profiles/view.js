3813def945b6de65d44515b36ea169a3583aa372
Marc Scheib <marc.scheib@gmx.de>
2015-12-06 23:06:57 +0100

    feat(profile): add basic view profile page
    Add the basic profile page to view profiles of users. Only allowes
    to view the profile of other users as a logged in user. Restricts the
    view from client side only.
    
    ---
    
     src/services/profiles/profile-service.js | 14 ++++++++++++++
     src/view-models/profiles/view.js         | 17 +++++++++++++++++
     src/views/profiles/view.html             | 11 +++++++++++
     3 files changed, 42 insertions(+)

---
+++ src/view-models/profiles/view.js
@@ -0,0 +1,17 @@
+import {inject} from 'aurelia-framework';
+import {ProfileService} from '../../services/profiles/profile-service';
+
+@inject(ProfileService)
+export class View {
+  constructor(profileService) {
+    this.profileService = profileService;
+  }
+
+  activate(params, routeConfig) {
+    return this.profileService.get(params.id)
+      .then(profile => {
+        this.profile = profile;
+        routeConfig.navModel.setTitle("Profile of " + profile.username);
+      });
+  }
+}
