6387a0c023943cb562b89d7799f3311176e085e3
Marc Scheib <marc.scheib@gmx.de>
2016-01-31 19:51:47 +0100

    chore(*): cleanup and update dependencies
    Remove unnecessary aurelia-loader, css, and process dependencies. Update
    to latest core-js version.
    
    ---
    
     config.js    | 46 ++--------------------------------------------
     package.json | 13 +++++--------
     2 files changed, 7 insertions(+), 52 deletions(-)

--- config.js
+++ config.js
@@ -18,13 +18,10 @@ System.config({
     "aurelia-api": "github:spoonx/aurelia-api@2.0.3",
     "aurelia-auth": "github:spoonx/aurelia-auth@0.13.2",
     "aurelia-bootstrapper": "npm:aurelia-bootstrapper@1.0.0-beta.1.1.1",
-    "aurelia-configuration": "github:vheissu/aurelia-configuration@1.0.3",
     "aurelia-event-aggregator": "npm:aurelia-event-aggregator@1.0.0-beta.1.1.0",
     "aurelia-fetch-client": "npm:aurelia-fetch-client@1.0.0-beta.1.1.0",
     "aurelia-framework": "npm:aurelia-framework@1.0.0-beta.1.1.1",
     "aurelia-history-browser": "npm:aurelia-history-browser@1.0.0-beta.1.1.1",
-    "aurelia-http-client": "github:aurelia/http-client@0.12.0",
-    "aurelia-loader": "npm:aurelia-loader@1.0.0-beta.1.1.0",
     "aurelia-loader-default": "npm:aurelia-loader-default@1.0.0-beta.1.1.1",
     "aurelia-logging-console": "npm:aurelia-logging-console@1.0.0-beta.1.1.3",
     "aurelia-router": "npm:aurelia-router@1.0.0-beta.1.1.0",
@@ -35,35 +32,13 @@ System.config({
     "aurelia-validation": "npm:aurelia-validation@0.6.1",
     "babel": "npm:babel-core@5.8.35",
     "babel-runtime": "npm:babel-runtime@5.8.35",
-    "core-js": "npm:core-js@1.2.6",
-    "css": "github:systemjs/plugin-css@0.1.20",
+    "core-js": "npm:core-js@2.0.3",
     "fetch": "github:github/fetch@0.10.1",
     "font-awesome": "github:FortAwesome/font-awesome@4.5.0",
     "jquery": "github:components/jquery@2.2.0",
     "nprogress": "github:rstacruz/nprogress@0.2.0",
-    "process": "npm:process@0.11.2",
-    "text": "github:systemjs/plugin-text@0.0.3",
+    "text": "github:systemjs/plugin-text@0.0.4",
     "twbs/bootstrap": "github:twbs/bootstrap@4.0.0-alpha",
-    "github:aurelia/dependency-injection@0.9.2": {
-      "aurelia-logging": "github:aurelia/logging@0.6.4",
-      "aurelia-metadata": "github:aurelia/metadata@0.7.3",
-      "core-js": "npm:core-js@0.9.18"
-    },
-    "github:aurelia/event-aggregator@0.7.0": {
-      "aurelia-logging": "github:aurelia/logging@0.6.4"
-    },
-    "github:aurelia/http-client@0.10.3": {
-      "aurelia-path": "github:aurelia/path@0.8.1",
-      "core-js": "npm:core-js@0.9.18"
-    },
-    "github:aurelia/http-client@0.12.0": {
-      "aurelia-pal": "github:aurelia/pal@0.2.0",
-      "aurelia-path": "github:aurelia/path@0.10.0",
-      "core-js": "npm:core-js@0.9.18"
-    },
-    "github:aurelia/metadata@0.7.3": {
-      "core-js": "npm:core-js@0.9.18"
-    },
     "github:jspm/nodelibs-assert@0.1.0": {
       "assert": "npm:assert@1.3.0"
     },
@@ -97,12 +72,6 @@ System.config({
     },
     "github:twbs/bootstrap@4.0.0-alpha": {
       "jquery": "github:components/jquery@2.2.0"
-    },
-    "github:vheissu/aurelia-configuration@1.0.3": {
-      "aurelia-dependency-injection": "github:aurelia/dependency-injection@0.9.2",
-      "aurelia-event-aggregator": "github:aurelia/event-aggregator@0.7.0",
-      "aurelia-http-client": "github:aurelia/http-client@0.10.3",
-      "core-js": "npm:core-js@0.9.18"
     },
     "npm:assert@1.3.0": {
       "util": "npm:util@0.10.3"
@@ -245,17 +214,6 @@ System.config({
     },
     "npm:babel-runtime@5.8.35": {
       "process": "github:jspm/nodelibs-process@0.1.2"
-    },
-    "npm:core-js@0.9.18": {
-      "fs": "github:jspm/nodelibs-fs@0.1.2",
-      "process": "github:jspm/nodelibs-process@0.1.2",
-      "systemjs-json": "github:systemjs/plugin-json@0.1.0"
-    },
-    "npm:core-js@1.2.6": {
-      "fs": "github:jspm/nodelibs-fs@0.1.2",
-      "path": "github:jspm/nodelibs-path@0.1.0",
-      "process": "github:jspm/nodelibs-process@0.1.2",
-      "systemjs-json": "github:systemjs/plugin-json@0.1.0"
     },
     "npm:core-js@2.0.3": {
       "fs": "github:jspm/nodelibs-fs@0.1.2",
