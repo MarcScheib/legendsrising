4b6ff2ab8b02926e7f0085a36707b96bfabebd10
Marc Scheib <marc.scheib@gmx.de>
2016-03-06 14:01:53 +0100

    chore(*): update changelog generation
    ---
    
     build/tasks/prepare-release.js | 20 +++++++++++---------
     package.json                   |  2 +-
     2 files changed, 12 insertions(+), 10 deletions(-)

--- build/tasks/prepare-release.js
+++ build/tasks/prepare-release.js
@@ -1,7 +1,7 @@
 var gulp = require('gulp');
 var runSequence = require('run-sequence');
 var paths = require('../paths');
-var changelog = require('conventional-changelog');
+var changelog = require('gulp-conventional-changelog');
 var fs = require('fs');
 var bump = require('gulp-bump');
 var args = require('../args');
@@ -16,16 +16,18 @@ gulp.task('bump-version', function () {
 
 // generates the CHANGELOG.md file based on commit
 // from git commit messages
-gulp.task('changelog', function (callback) {
+gulp.task('changelog', function () {
   var pkg = JSON.parse(fs.readFileSync('./package.json', 'utf-8'));
 
-  return changelog({
-    repository: pkg.repository.url,
-    version: pkg.version,
-    file: paths.doc + '/CHANGELOG.md'
-  }, function (err, log) {
-    fs.writeFileSync(paths.doc + '/CHANGELOG.md', log);
-  });
+  return gulp.src(paths.doc + '/CHANGELOG.md', {
+      buffer: false
+    })
+    .pipe(changelog({
+      preset: 'angular',
+      repository: pkg.repository.url,
+      version: pkg.version
+    }))
+    .pipe(gulp.dest(paths.doc + '/'));
 });
 
 // calls the listed sequence of tasks in order
