cb668ad883744c1cefbcf16f1aa2224c596f34f5
Marc Scheib <marc.scheib@gmx.de>
2016-04-17 20:37:29 +0200

    test(e2e): add simple e2e spec
    ---
    
     test/e2e/src/index.spec.js  | 12 +++++++++++-
     test/e2e/src/skeleton.po.js | 13 +++++++++++++
     2 files changed, 24 insertions(+), 1 deletion(-)

--- test/e2e/src/index.spec.js
+++ test/e2e/src/index.spec.js
@@ -1,5 +1,15 @@
-describe('LegendsRising app', () => {
+import {PageObjectSkeleton} from './skeleton.po.js';
+
+describe('LegendsRising app', function() {
+  let poSkeleton;
+
   beforeEach(() => {
+    poSkeleton = new PageObjectSkeleton();
+
     browser.loadAndWaitForApp('http://localhost:9000');
+  });
+
+  it('should load the page and display the initial page title', () => {
+    expect(poSkeleton.getCurrentPageTitle()).toBe('Welcome | LegendsRising');
   });
 });
---
+++ test/e2e/src/skeleton.po.js
@@ -0,0 +1,13 @@
+export class PageObjectSkeleton {
+  constructor() {
+  }
+
+  getCurrentPageTitle() {
+    return browser.getTitle();
+  }
+
+  navigateTo(href) {
+    element(by.css('a[href="' + href + '"]')).click();
+    return browser.waitForRouterComplete();
+  }
+}
