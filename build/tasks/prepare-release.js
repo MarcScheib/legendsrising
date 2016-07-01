3364a926771c1c2d4b226024932ee8b65fa46cea
Marc Scheib <marc.scheib@gmx.de>
2016-07-01 20:51:34 +0200

    chore(*): update changelog task

--- build/tasks/prepare-release.js
+++ build/tasks/prepare-release.js
@@ -6,31 +6,21 @@ var fs = require('fs');
 var bump = require('gulp-bump');
 var args = require('../args');
 
-// utilizes the bump plugin to bump the
-// semver for the repo
 gulp.task('bump-version', function () {
   return gulp.src(['./package.json'], {base: './'})
     .pipe(bump({type: args.bump})) //major|minor|patch|prerelease
     .pipe(gulp.dest('./'));
 });
 
-// generates the CHANGELOG.md file based on commit
-// from git commit messages
-gulp.task('changelog', function () {
-  var pkg = JSON.parse(fs.readFileSync('./package.json', 'utf-8'));
-
+gulp.task('changelog', function() {
   return gulp.src(paths.doc + '/CHANGELOG.md', {
-      buffer: false
-    })
-    .pipe(changelog({
-      preset: 'angular',
-      repository: pkg.repository.url,
-      version: pkg.version
-    }))
-    .pipe(gulp.dest(paths.doc + '/'));
+    buffer: false
+  }).pipe(changelog({
+    preset: 'angular'
+  }))
+    .pipe(gulp.dest(paths.doc));
 });
 
-// calls the listed sequence of tasks in order
 gulp.task('prepare-release', function (callback) {
   return runSequence(
     'build',
