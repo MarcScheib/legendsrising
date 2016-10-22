811780370bfc63291b6e663d7c4497f31b4c7e8f
Marc Scheib <marc.scheib@gmx.de>
2016-10-22 21:26:14 +0200

    fix(router): fix scroll to top on routing
    Scroll to top works on document body. <main> element is scrolled
    because of new document style. Change scrolling to top to scroll
    back <main> element instead of <body>.

--- src/configuration/router/pipeline/ScrollToTop.js
+++ src/configuration/router/pipeline/ScrollToTop.js
@@ -1,6 +1,21 @@
 export class ScrollToTop {
   run(instruction, next) {
-    document.body.scrollTop = 0;
+    this.scrollSmooth();
     return next();
+  }
+
+  scrollSmooth() {
+    let element = document.getElementsByTagName('main')[0];
+    let movingFrequency = 15;
+    let hops = 33;
+    let gap = element.scrollTop / hops;
+
+    for (var i = 1; i <= hops; i++) {
+      (function() {
+        setTimeout(() => {
+          element.scrollTop = Math.max(element.scrollTop - gap, 0);
+        }, movingFrequency * i);
+      })();
+    }
   }
 }
