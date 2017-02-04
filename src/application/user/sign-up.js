b6fee7d899c51bc49069f159f432c2042b22a6b8
Marc Scheib <marc.scheib@gmx.de>
2017-02-04 11:59:11 +0100

    fix(sign-up): add catch clauses if service is unavailable

--- src/application/user/sign-up.js
+++ src/application/user/sign-up.js
@@ -1,9 +1,9 @@
-import {inject, NewInstance} from 'aurelia-framework';
-import {NotificationService} from 'aurelia-notify';
-import {Router} from 'aurelia-router';
-import {ValidationController, ValidationRules, validateTrigger} from 'aurelia-validation';
-import {BootstrapFormRenderer} from '../../resources/validation/bootstrap-form-renderer';
-import {UserService} from '../../services/users/user-service';
+import {inject, NewInstance} from "aurelia-framework";
+import {NotificationService} from "aurelia-notify";
+import {Router} from "aurelia-router";
+import {ValidationController, ValidationRules, validateTrigger} from "aurelia-validation";
+import {BootstrapFormRenderer} from "../../resources/validation/bootstrap-form-renderer";
+import {UserService} from "../../services/users/user-service";
 
 @inject(Router, NewInstance.of(ValidationController), UserService, NotificationService)
 export class SignUp {
@@ -22,13 +22,18 @@ export class SignUp {
       .then()
       .satisfies(newValue => {
         return new Promise((accept, reject) => {
-          this.userService.isUsernameExisting(newValue).then(data => {
-            if (data.exists) {
+          this.userService.isUsernameExisting(newValue)
+            .then(data => {
+              if (data.exists) {
+                accept(false);
+              } else {
+                accept(true);
+              }
+            })
+            .catch(data => {
+              this.notification.danger('There seems to be an issue with the user service. Please, try again.');
               accept(false);
-            } else {
-              accept(true);
-            }
-          });
+            });
         });
       }).withMessage(`\${$displayName} is already taken.`)
       .ensure('email')
@@ -37,20 +42,25 @@ export class SignUp {
       .then()
       .satisfies(newValue => {
         return new Promise((accept, reject) => {
-          this.userService.isEmailExisting(newValue).then(data => {
-            if (data.exists) {
+          this.userService.isEmailExisting(newValue)
+            .then(data => {
+              if (data.exists) {
+                accept(false);
+              } else {
+                accept(true);
+              }
+            })
+            .catch(data => {
+              this.notification.danger('There seems to be an issue with the user service. Please, try again.');
               accept(false);
-            } else {
-              accept(true);
-            }
-          });
+            });
         });
       }).withMessage(`\${$displayName} is already taken.`)
       .ensure('password')
       .required()
       .minLength(8)
       .maxLength(32)
-      .ensure('password_repeat')
+      .ensure('password_repeat').displayName('Password')
       .required()
       .satisfies(newValue => {
         return newValue === this.password;
@@ -87,7 +97,7 @@ export class SignUp {
               }
             })
             .catch(data => {
-              this.notification.danger('You have got errors in your sign up form.');
+              this.notification.danger('The sign up process failed. Please try again.');
             });
         } else {
           this.notification.danger('You have got errors in your sign up form.');
