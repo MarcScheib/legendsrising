2e831bd06b6119ca8aefb1af84db8f81c6c7259f
Marc Scheib <marc.scheib@gmx.de>
2017-02-12 13:59:13 +0100

    test(faq): add test case

--- test/unit/application/faq.spec.js
+++ test/unit/application/faq.spec.js
@@ -13,4 +13,35 @@ describe('the FAQ Index module', () => {
     faqService = new FaqServiceStub();
     sut = new Index(faqService);
   });
+
+  it('contains a faq service property', () => {
+    expect(sut.faqService).toBeDefined();
+  });
+
+  it('sets fetch response to faqs', done => {
+    faqService.itemStub = { data: itemStubs };
+    sut.activate()
+      .then(() => {
+        expect(sut.faqs).toBe(itemStubs);
+        expect(sut.faqs).not.toBe(itemFake);
+        done();
+      })
+      .catch(result => {
+        expect(result).not.toBe(result);
+        done();
+      });
+  });
+
+  it('contains an empty list on api fail', done => {
+    faqService.reject = true;
+    sut.activate()
+      .then(() => {
+        expect(sut.faqs).toEqual([]);
+        done();
+      })
+      .catch(result => {
+        expect(result).not.toBe(result);
+        done();
+      });
+  });
 });
