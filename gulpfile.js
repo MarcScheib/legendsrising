06655e0443b9d127c137083bc6273787b5b24dce
Marc Scheib <marc.scheib@gmx.de>
2017-07-01 16:38:08 +0200

    chore(*): remove gulp build tasks

--- gulpfile.js
+++
@@ -1,5 +0,0 @@
-// all gulp tasks are located in the ./build/tasks directory
-// gulp configuration is located in files in ./build directory
-var requireDir = require('require-dir');
-
-requireDir('build/tasks');
