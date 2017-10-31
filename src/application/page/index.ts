1d37d363502b4df234e1097a8ded872b548d8fb8
Marc Scheib <marc.scheib@gmx.de>
2017-10-31 13:48:40 +0100

    fix(webpack): fix dynamic page loading

--- src/application/page/index.ts
+++ src/application/page/index.ts
@@ -1,18 +1,22 @@
 import { activationStrategy } from 'aurelia-router';
 import { _hyphenate, _titlecase } from '../../utilities/util';
+import { PLATFORM } from 'aurelia-pal';
+
+const pages = {
+  'legal-notice': PLATFORM.moduleName('application/page/legal-notice.html'),
+  'pricing': PLATFORM.moduleName('application/page/pricing.html'),
+  'story': PLATFORM.moduleName('application/page/story.html')
+};
 
 export class Index {
   view: string;
-
-  constructor() {
-  }
 
   determineActivationStrategy() {
     return activationStrategy.replace;
   }
 
   getViewStrategy() {
-    return 'application/page/' + this.view + '.html';
+    return pages[this.view];
   }
 
   activate(params, routeConfig) {
