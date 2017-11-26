a86028459cb8a03a515ecd5635b2f110c4069c8c
Marc Scheib <marc.scheib@gmx.de>
2017-11-26 15:36:19 +0100

    chore(*): fix travis ci

--- test/unit/configuration/router-config.spec.ts
+++ test/unit/configuration/router-config.spec.ts
@@ -58,63 +58,75 @@ describe('the router configuration', () => {
           moduleId: 'application/content/index',
           title: 'Welcome'
         });
-      })
-      .catch(() => {
-        expect(true).toBeFalsy();
       });
   });
 
   it('should have a news list route', () => {
-    expect(sut.router.routes).toContain({
-      route: ['news', 'news/index'],
-      moduleId: 'application/news/index',
-      title: 'News'
-    });
+    sut.configure()
+      .then(() => {
+        expect(sut.router.routes).toContain({
+          route: ['news', 'news/index'],
+          moduleId: 'application/news/index',
+          title: 'News'
+        });
+      });
   });
 
   it('should have a news view route', () => {
-    expect(sut.router.routes).toContain({
-      route: ['news/view/:id'],
-      moduleId: 'application/news/view',
-      title: 'News'
-    });
+    sut.configure()
+      .then(() => {
+        expect(sut.router.routes).toContain({
+          route: ['news/view/:id'],
+          moduleId: 'application/news/view',
+          title: 'News'
+        });
+      });
   });
 
   it('should have auth routes', () => {
-    expect(sut.router.routes).toContain({
-      route: ['user/signup'],
-      moduleId: 'application/user/sign-up',
-      title: 'Sign Up'
-    });
-    expect(sut.router.routes).toContain({
-      route: ['auth/signin'],
-      moduleId: 'application/auth/sign-in',
-      title: 'Sign In'
-    });
-    expect(sut.router.routes).toContain({
-      route: ['auth/signout'],
-      moduleId: 'application/auth/sign-out',
-      title: 'Sign Out'
-    });
+    sut.configure()
+      .then(() => {
+        expect(sut.router.routes).toContain({
+          route: ['user/signup'],
+          moduleId: 'application/user/sign-up',
+          title: 'Sign Up'
+        });
+        expect(sut.router.routes).toContain({
+          route: ['auth/signin'],
+          moduleId: 'application/auth/sign-in',
+          title: 'Sign In'
+        });
+        expect(sut.router.routes).toContain({
+          route: ['auth/signout'],
+          moduleId: 'application/auth/sign-out',
+          title: 'Sign Out'
+        });
+      });
   });
 
   it('should have profile routes', () => {
-    expect(sut.router.routes).toContain({
-      route: ['profile/view/:id'],
-      moduleId: 'application/profile/view',
-      title: 'Profile',
-      auth: true
-    });
-    expect(sut.router.routes).toContain({
-      route: ['profile/edit'],
-      moduleId: 'application/profile/edit',
-      title: 'Edit Profile',
-      auth: true
-    });
+    sut.configure()
+      .then(() => {
+        expect(sut.router.routes).toContain({
+          route: ['profile/view/:id'],
+          moduleId: 'application/profile/view',
+          title: 'Profile',
+          auth: true
+        });
+        expect(sut.router.routes).toContain({
+          route: ['profile/edit'],
+          moduleId: 'application/profile/edit',
+          title: 'Edit Profile',
+          auth: true
+        });
+      });
   });
 
   it('should have a unknown route config', () => {
-    expect(sut.router.unknownRouteConfig).toEqual(jasmine.any(String));
-    expect(sut.router.unknownRouteConfig).toEqual('application/error/error404');
+    sut.configure()
+      .then(() => {
+        expect(sut.router.unknownRouteConfig).toEqual(jasmine.any(String));
+        expect(sut.router.unknownRouteConfig).toEqual('application/error/error404');
+      });
   });
 });
