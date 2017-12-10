5b6d1467458e811a4845f1f36f1e08df33d74f28
Marc Scheib <marc.scheib@gmx.de>
2017-12-10 14:55:48 +0100

    fix(persistence): make resource name optional

--- src/resources/features/persistence/decorator/resource.ts
+++ src/resources/features/persistence/decorator/resource.ts
@@ -4,11 +4,11 @@ import { Entity } from '../entity';
 /**
  * Sets the 'resource' metadata on the entity.
  *
- * @param {string} resourceName - The name of the resource
+ * @param {string} [resourceName] - The name of the resource
  * @return {function}
  * @decorator
  */
-export function resource(resourceName: string): (target: any) => void {
+export function resource(resourceName?: string): (target: any) => void {
   return function <T extends typeof Entity>(target: T): void {
     const object: any = metadata.getOrCreateOwn(metadata.paramTypes, Map, target, target.name);
     object.set('resource', resourceName || target.name.toLowerCase());
